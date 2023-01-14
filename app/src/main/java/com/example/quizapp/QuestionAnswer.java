d64d9fdd7bf96e4f8d504f88def311cc835fbe30
SahilSingh999 <sahil365ind@gmail.com>
2023-01-14 18:54:37 +0530

    task 4 android development internship

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,30 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which company owns the Android?",
+            "Who created 1st Android phone?",
+            "Android is based on which language?",
+            "Google purchased Android in which of these years?",
+            "Which of these does NOT refer to a nickname of the Android version?"
+
+    };
+
+    public static String choices[][] ={
+            {"Google","Apple","Nokia","Samsung"},
+            {"HTC","Nokia","Samsung","Motorola"},
+            {"Java","C++","HTML","None"},
+            {"2010","2008","2005","2003"},
+            {"HoneyComb","GingerBread","Cupcake","Muffin"}
+    };
+
+    public static String correctAnswers[] ={
+            "Google",
+            "HTC",
+            "Java",
+            "2005",
+            "Muffin"
+
+    };
+}
